a756f2584b2f7924a085918916ab7e007b99ff99
Caroline <38522055+carolinechoi@users.noreply.github.com>
2019-07-09 23:58:57 -0700

    added vuejs

--- vuejs/main.js
+++ vuejs/main.js
@@ -1,6 +1,61 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello World!'
+        message: "Hello world! It's currently " + new Date().toLocaleDateString()
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString(),
+        seen: true
+    }
+})
+
+var app3 = new Vue({
+    el: '#for-directive',
+    data: {
+        list: [
+            { name: 'St. Vincent' },
+            { name: 'Beyonce' }
+        ]
+    }
+})
+
+var app4 = new Vue({
+    el: '#button',
+    data: {
+        message: 'Hello Caroline!'
+    },
+    methods: {
+        reverseMessage: function(){
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+var app5 = new Vue({
+    el: '#inputs',
+    data: {
+        message: 'Enter something.'
+    }
+})
+
+app3.list.push({ name: 'Billie Eilish '})
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var component = new Vue({
+    el: '#component',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Himalayan Salt' },
+            { id: 2, text: 'Whatever else' }
+        ]
     }
 })
---
+++ vuejs/tests/project-1.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#input',
+    data: {
+        name: '',
+        artist1: '',
+        artist2: '',
+        artist3: ''
+    }
+})
